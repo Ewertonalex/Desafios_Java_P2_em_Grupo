2dd6f8e7c460ac4221e9985a3f5535ba4514891c
Hugo Facundo <89493574+hufacundo@users.noreply.github.com>
2022-05-18 20:15:21 -0300

    Update SecaoDetalhe.java

--- KeepFiles/src/SecaoDetalhe.java
+++ KeepFiles/src/SecaoDetalhe.java
@@ -30,6 +30,38 @@ public class SecaoDetalhe {
     private String Folder;
     private String CodPab;
     private String NomeAdicional;
+    private String EnderecoAge;
+    private String BairroAge;
+    private String CidadeAge;
+    private String UfAge;
+    private String CepAge;
+    private String ContaCorrente;
+    private String Tm;
+    private String Templete;
+    private String Tm2;
+    private String Complemento;
+    private String CodPlast;
+    private String Empresa;
+    private String Foto;
+    private String VlLimite;
+    private String DiaVenc;
+    private String CodigoBarras;
+    private String DevicienteVisual;
+    private String Senha;
+    private String Trilha01Icvv;
+    private String Trilha02Icvv;
+    private String CcrAntigo;
+    private String CodigoCartao;
+    private String NomeOrigemComercial;
+    private String Matricula;
+    private String Cargo;
+    private String Documento;
+    private String Ddd;
+    private String Telefone;
+    private String Vago;
+    private String Panvas;
+    private String NumeroEndereco;
+    private String ComplementoEndereco;
 
 
 
