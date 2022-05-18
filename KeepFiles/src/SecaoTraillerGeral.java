6c47efeb794116018af6c42dc1d3e7b3953aad8c
Luan Antony <luanantony29@gmail.com>
2022-05-18 19:35:47 -0300

    Criando Variaveis

--- KeepFiles/src/SecaoTraillerGeral.java
+++ KeepFiles/src/SecaoTraillerGeral.java
@@ -1,2 +1,4 @@
 public class SecaoTraillerGeral {
+    private String TipReg;
+    private String QtdCartArq;
 }
--- KeepFiles/src/SecaoTraillerLote.java
+++ KeepFiles/src/SecaoTraillerLote.java
@@ -1,2 +1,7 @@
 public class SecaoTraillerLote {
+    private String TipReg;
+    private String ModPlast;
+    private String Kit;
+    private String QtdCartLote;
+
 }
