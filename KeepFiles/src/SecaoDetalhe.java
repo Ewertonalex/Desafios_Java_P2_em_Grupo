1b8330bfc30de5ccc99b09d98e81a3e81b4fde20
Carlos Augusto Alves B de Santana <94987619+caalvesbs@users.noreply.github.com>
2022-05-18 20:06:15 -0300

    Criando classe detalhe

--- KeepFiles/src/SecaoDetalhe.java
+++ KeepFiles/src/SecaoDetalhe.java
@@ -1,2 +1,36 @@
 public class SecaoDetalhe {
+
+    private String TipRegDetalhe;
+    private String ModPlast;
+    private String Kit;
+    private String SeqCart;
+    private String Linha01;
+    private String Linha02;
+    private String Linha03;
+    private String Linha04;
+    private String LinhaVerso;
+    private String Trilha01;
+    private String Trilha02;
+    private String Trilha03;
+    private String QtdCartoes;
+    private String Nome;
+    private String DadosEndereco;
+    private String Bairro;
+    private String Cidade;
+    private String Uf;
+    private String Cep;
+    private String NroAr;
+    private String CepAr;
+    private String CodRegional;
+    private String CodAgencia; 
+    private String NmAbrAge;
+    private String TpUorPab;
+    private String CodUorPab;
+    private String NmAbrPab;
+    private String Folder;
+    private String CodPab;
+    private String NomeAdicional;
+
+
+
 }
