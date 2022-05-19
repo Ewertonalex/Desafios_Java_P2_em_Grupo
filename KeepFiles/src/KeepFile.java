5056ac3272af06d8958284634dfd625c8b8a0051
Luan Antony <luanantony29@gmail.com>
2022-05-19 20:09:14 -0300

    Commit

--- KeepFiles/src/KeepFile.java
+++ KeepFiles/src/KeepFile.java
@@ -2,18 +2,29 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class KeepFile {
     public static void main(String[] args) throws FileNotFoundException {
 
         String path = "C:\\GOLD_180422.txt";
-
+        ArrayList<String> listaErros = new ArrayList<String>();
         try (BufferedReader br = new BufferedReader(new FileReader(path))){
 
             String line = br.readLine();
 
             while(line != null) {
-                System.out.println(line.substring(0,2));
+                if(line.substring(0,1).equals("0")){
+                    SecaoGeral secaoGeral = new SecaoGeral(line);
+                    System.out.println(secaoGeral.getBanco());
+                    System.out.println(secaoGeral.getSistOrig());
+
+                    secaoGeral.validarLinha(secaoGeral,listaErros);
+                }else
+                    if(line.substring(0,1).equals("1")){
+
+                    }
+//                System.out.println(line.substring(0,2));
 
                 line = br.readLine();
             }
--- KeepFiles/src/SecaoGeral.java
+++ KeepFiles/src/SecaoGeral.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SecaoGeral {
     private String TipRegHg;
     private String Banco;
@@ -51,5 +53,25 @@ public class SecaoGeral {
 
     public void setDtMovto(String dtMovto) {
         DtMovto = dtMovto;
+    }
+
+    public void validarLinha(SecaoGeral secaoGeral, ArrayList<String> listaErros) {
+        if(isInteger(secaoGeral.Nsp)){
+            try{
+                int i = Integer.parseInt(secaoGeral.Nsp);
+            }catch (Exception e){
+                e.getMessage();
+                listaErros.add("Linha secao: ["+secaoGeral.getTipRegHg()+"] - Esperava int e contem String - valor: "+secaoGeral.getNsp());
+            }
+
+            listaErros.add(("Problema no arquivo XTOP, na secao geral, na com campo NSP "));
+        }
+
+        secaoGeral.getNsp(); //voces precisam buscar como validar campo numerico.
+
+
+    }
+    private static boolean isInteger(String str) {
+        return str != null && str.matches("[0-9]*");
     }
 }
