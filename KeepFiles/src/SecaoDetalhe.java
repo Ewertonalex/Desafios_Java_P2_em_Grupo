8f1fbd1336d71220eddee02204edd675d69c51e0
Luan Antony <luanantony29@gmail.com>
2022-05-19 21:14:14 -0300

    Revert SecaoDetalhe

--- KeepFiles/src/SecaoDetalhe.java
+++ KeepFiles/src/SecaoDetalhe.java
@@ -1,1 +1,637 @@
+public class SecaoDetalhe {
 
+    private String TipRegDetalhe;
+    private String ModPlast;
+    private String Kit;
+    private String SeqCart;
+    private String Linha01;
+    private String Linha02;
+    private String Linha03;
+    private String Linha04;
+    private String LinhaVerso;
+    private String Trilha01;
+    private String Trilha02;
+    private String Trilha03;
+    private String QtdCartoes;
+    private String Nome;
+    private String DadosEndereco;
+    private String Bairro;
+    private String Cidade;
+    private String Uf;
+    private String Cep;
+    private String NroAr;
+    private String CepAr;
+    private String CodRegional;
+    private String CodAgencia;
+    private String NmAbrAge;
+    private String TpUorPab;
+    private String CodUorPab;
+    private String NmAbrPab;
+    private String Folder;
+    private String CodPab;
+    private String NomeAdicional;
+    private String EnderecoAge;
+    private String BairroAge;
+    private String CidadeAge;
+    private String UfAge;
+    private String CepAge;
+    private String ContaCorrente;
+    private String Tm;
+    private String Templete;
+    private String Tm2;
+    private String Complemento;
+    private String CodPlast;
+    private String Empresa;
+    private String Foto;
+    private String VlLimite;
+    private String DiaVenc;
+    private String CodigoBarras;
+    private String DevicienteVisual;
+    private String Senha;
+    private String Trilha01Icvv;
+    private String Trilha02Icvv;
+    private String CcrAntigo;
+    private String CodigoCartao;
+    private String NomeOrigemComercial;
+    private String Matricula;
+    private String Cargo;
+    private String Documento;
+    private String Ddd;
+    private String Telefone;
+    private String Vago;
+    private String Panvas;
+    private String NumeroEndereco;
+    private String ComplementoEndereco;
+
+    public SecaoDetalhe(String tipRegDetalhe, String modPlast, String kit, String seqCart, String linha01,
+                        String linha02, String linha03, String linha04, String linhaVerso, String trilha01,
+                        String trilha02, String trilha03, String qtdCartoes, String nome, String dadosEndereco,
+                        String bairro, String cidade, String uf, String cep, String nroAr, String cepAr,
+                        String codRegional, String codAgencia, String nmAbrAge, String tpUorPab, String codUorPab,
+                        String nmAbrPab, String folder, String codPab, String nomeAdicional, String enderecoAge,
+                        String bairroAge, String cidadeAge, String ufAge, String cepAge, String contaCorrente,
+                        String tm, String templete, String tm2, String complemento, String codPlast, String empresa,
+                        String foto, String vlLimite, String diaVenc, String codigoBarras, String devicienteVisual,
+                        String senha, String trilha01Icvv, String trilha02Icvv, String ccrAntigo, String codigoCartao,
+                        String nomeOrigemComercial, String matricula, String cargo, String documento, String ddd,
+                        String telefone, String vago, String panvas, String numeroEndereco, String complementoEndereco) {
+        TipRegDetalhe = tipRegDetalhe;
+        ModPlast = modPlast;
+        Kit = kit;
+        SeqCart = seqCart;
+        Linha01 = linha01;
+        Linha02 = linha02;
+        Linha03 = linha03;
+        Linha04 = linha04;
+        LinhaVerso = linhaVerso;
+        Trilha01 = trilha01;
+        Trilha02 = trilha02;
+        Trilha03 = trilha03;
+        QtdCartoes = qtdCartoes;
+        Nome = nome;
+        DadosEndereco = dadosEndereco;
+        Bairro = bairro;
+        Cidade = cidade;
+        Uf = uf;
+        Cep = cep;
+        NroAr = nroAr;
+        CepAr = cepAr;
+        CodRegional = codRegional;
+        CodAgencia = codAgencia;
+        NmAbrAge = nmAbrAge;
+        TpUorPab = tpUorPab;
+        CodUorPab = codUorPab;
+        NmAbrPab = nmAbrPab;
+        Folder = folder;
+        CodPab = codPab;
+        NomeAdicional = nomeAdicional;
+        EnderecoAge = enderecoAge;
+        BairroAge = bairroAge;
+        CidadeAge = cidadeAge;
+        UfAge = ufAge;
+        CepAge = cepAge;
+        ContaCorrente = contaCorrente;
+        Tm = tm;
+        Templete = templete;
+        Tm2 = tm2;
+        Complemento = complemento;
+        CodPlast = codPlast;
+        Empresa = empresa;
+        Foto = foto;
+        VlLimite = vlLimite;
+        DiaVenc = diaVenc;
+        CodigoBarras = codigoBarras;
+        DevicienteVisual = devicienteVisual;
+        Senha = senha;
+        Trilha01Icvv = trilha01Icvv;
+        Trilha02Icvv = trilha02Icvv;
+        CcrAntigo = ccrAntigo;
+        CodigoCartao = codigoCartao;
+        NomeOrigemComercial = nomeOrigemComercial;
+        Matricula = matricula;
+        Cargo = cargo;
+        Documento = documento;
+        Ddd = ddd;
+        Telefone = telefone;
+        Vago = vago;
+        Panvas = panvas;
+        NumeroEndereco = numeroEndereco;
+        ComplementoEndereco = complementoEndereco;
+    }
+
+    public String getTipRegDetalhe() {
+        return TipRegDetalhe;
+    }
+
+    public void setTipRegDetalhe(String tipRegDetalhe) {
+        TipRegDetalhe = tipRegDetalhe;
+    }
+
+    public String getModPlast() {
+        return ModPlast;
+    }
+
+    public void setModPlast(String modPlast) {
+        ModPlast = modPlast;
+    }
+
+    public String getKit() {
+        return Kit;
+    }
+
+    public void setKit(String kit) {
+        Kit = kit;
+    }
+
+    public String getSeqCart() {
+        return SeqCart;
+    }
+
+    public void setSeqCart(String seqCart) {
+        SeqCart = seqCart;
+    }
+
+    public String getLinha01() {
+        return Linha01;
+    }
+
+    public void setLinha01(String linha01) {
+        Linha01 = linha01;
+    }
+
+    public String getLinha02() {
+        return Linha02;
+    }
+
+    public void setLinha02(String linha02) {
+        Linha02 = linha02;
+    }
+
+    public String getLinha03() {
+        return Linha03;
+    }
+
+    public void setLinha03(String linha03) {
+        Linha03 = linha03;
+    }
+
+    public String getLinha04() {
+        return Linha04;
+    }
+
+    public void setLinha04(String linha04) {
+        Linha04 = linha04;
+    }
+
+    public String getLinhaVerso() {
+        return LinhaVerso;
+    }
+
+    public void setLinhaVerso(String linhaVerso) {
+        LinhaVerso = linhaVerso;
+    }
+
+    public String getTrilha01() {
+        return Trilha01;
+    }
+
+    public void setTrilha01(String trilha01) {
+        Trilha01 = trilha01;
+    }
+
+    public String getTrilha02() {
+        return Trilha02;
+    }
+
+    public void setTrilha02(String trilha02) {
+        Trilha02 = trilha02;
+    }
+
+    public String getTrilha03() {
+        return Trilha03;
+    }
+
+    public void setTrilha03(String trilha03) {
+        Trilha03 = trilha03;
+    }
+
+    public String getQtdCartoes() {
+        return QtdCartoes;
+    }
+
+    public void setQtdCartoes(String qtdCartoes) {
+        QtdCartoes = qtdCartoes;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getDadosEndereco() {
+        return DadosEndereco;
+    }
+
+    public void setDadosEndereco(String dadosEndereco) {
+        DadosEndereco = dadosEndereco;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String bairro) {
+        Bairro = bairro;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getUf() {
+        return Uf;
+    }
+
+    public void setUf(String uf) {
+        Uf = uf;
+    }
+
+    public String getCep() {
+        return Cep;
+    }
+
+    public void setCep(String cep) {
+        Cep = cep;
+    }
+
+    public String getNroAr() {
+        return NroAr;
+    }
+
+    public void setNroAr(String nroAr) {
+        NroAr = nroAr;
+    }
+
+    public String getCepAr() {
+        return CepAr;
+    }
+
+    public void setCepAr(String cepAr) {
+        CepAr = cepAr;
+    }
+
+    public String getCodRegional() {
+        return CodRegional;
+    }
+
+    public void setCodRegional(String codRegional) {
+        CodRegional = codRegional;
+    }
+
+    public String getCodAgencia() {
+        return CodAgencia;
+    }
+
+    public void setCodAgencia(String codAgencia) {
+        CodAgencia = codAgencia;
+    }
+
+    public String getNmAbrAge() {
+        return NmAbrAge;
+    }
+
+    public void setNmAbrAge(String nmAbrAge) {
+        NmAbrAge = nmAbrAge;
+    }
+
+    public String getTpUorPab() {
+        return TpUorPab;
+    }
+
+    public void setTpUorPab(String tpUorPab) {
+        TpUorPab = tpUorPab;
+    }
+
+    public String getCodUorPab() {
+        return CodUorPab;
+    }
+
+    public void setCodUorPab(String codUorPab) {
+        CodUorPab = codUorPab;
+    }
+
+    public String getNmAbrPab() {
+        return NmAbrPab;
+    }
+
+    public void setNmAbrPab(String nmAbrPab) {
+        NmAbrPab = nmAbrPab;
+    }
+
+    public String getFolder() {
+        return Folder;
+    }
+
+    public void setFolder(String folder) {
+        Folder = folder;
+    }
+
+    public String getCodPab() {
+        return CodPab;
+    }
+
+    public void setCodPab(String codPab) {
+        CodPab = codPab;
+    }
+
+    public String getNomeAdicional() {
+        return NomeAdicional;
+    }
+
+    public void setNomeAdicional(String nomeAdicional) {
+        NomeAdicional = nomeAdicional;
+    }
+
+    public String getEnderecoAge() {
+        return EnderecoAge;
+    }
+
+    public void setEnderecoAge(String enderecoAge) {
+        EnderecoAge = enderecoAge;
+    }
+
+    public String getBairroAge() {
+        return BairroAge;
+    }
+
+    public void setBairroAge(String bairroAge) {
+        BairroAge = bairroAge;
+    }
+
+    public String getCidadeAge() {
+        return CidadeAge;
+    }
+
+    public void setCidadeAge(String cidadeAge) {
+        CidadeAge = cidadeAge;
+    }
+
+    public String getUfAge() {
+        return UfAge;
+    }
+
+    public void setUfAge(String ufAge) {
+        UfAge = ufAge;
+    }
+
+    public String getCepAge() {
+        return CepAge;
+    }
+
+    public void setCepAge(String cepAge) {
+        CepAge = cepAge;
+    }
+
+    public String getContaCorrente() {
+        return ContaCorrente;
+    }
+
+    public void setContaCorrente(String contaCorrente) {
+        ContaCorrente = contaCorrente;
+    }
+
+    public String getTm() {
+        return Tm;
+    }
+
+    public void setTm(String tm) {
+        Tm = tm;
+    }
+
+    public String getTemplete() {
+        return Templete;
+    }
+
+    public void setTemplete(String templete) {
+        Templete = templete;
+    }
+
+    public String getTm2() {
+        return Tm2;
+    }
+
+    public void setTm2(String tm2) {
+        Tm2 = tm2;
+    }
+
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        Complemento = complemento;
+    }
+
+    public String getCodPlast() {
+        return CodPlast;
+    }
+
+    public void setCodPlast(String codPlast) {
+        CodPlast = codPlast;
+    }
+
+    public String getEmpresa() {
+        return Empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        Empresa = empresa;
+    }
+
+    public String getFoto() {
+        return Foto;
+    }
+
+    public void setFoto(String foto) {
+        Foto = foto;
+    }
+
+    public String getVlLimite() {
+        return VlLimite;
+    }
+
+    public void setVlLimite(String vlLimite) {
+        VlLimite = vlLimite;
+    }
+
+    public String getDiaVenc() {
+        return DiaVenc;
+    }
+
+    public void setDiaVenc(String diaVenc) {
+        DiaVenc = diaVenc;
+    }
+
+    public String getCodigoBarras() {
+        return CodigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        CodigoBarras = codigoBarras;
+    }
+
+    public String getDevicienteVisual() {
+        return DevicienteVisual;
+    }
+
+    public void setDevicienteVisual(String devicienteVisual) {
+        DevicienteVisual = devicienteVisual;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
+
+    public String getTrilha01Icvv() {
+        return Trilha01Icvv;
+    }
+
+    public void setTrilha01Icvv(String trilha01Icvv) {
+        Trilha01Icvv = trilha01Icvv;
+    }
+
+    public String getTrilha02Icvv() {
+        return Trilha02Icvv;
+    }
+
+    public void setTrilha02Icvv(String trilha02Icvv) {
+        Trilha02Icvv = trilha02Icvv;
+    }
+
+    public String getCcrAntigo() {
+        return CcrAntigo;
+    }
+
+    public void setCcrAntigo(String ccrAntigo) {
+        CcrAntigo = ccrAntigo;
+    }
+
+    public String getCodigoCartao() {
+        return CodigoCartao;
+    }
+
+    public void setCodigoCartao(String codigoCartao) {
+        CodigoCartao = codigoCartao;
+    }
+
+    public String getNomeOrigemComercial() {
+        return NomeOrigemComercial;
+    }
+
+    public void setNomeOrigemComercial(String nomeOrigemComercial) {
+        NomeOrigemComercial = nomeOrigemComercial;
+    }
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        Matricula = matricula;
+    }
+
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String cargo) {
+        Cargo = cargo;
+    }
+
+    public String getDocumento() {
+        return Documento;
+    }
+
+    public void setDocumento(String documento) {
+        Documento = documento;
+    }
+
+    public String getDdd() {
+        return Ddd;
+    }
+
+    public void setDdd(String ddd) {
+        Ddd = ddd;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getVago() {
+        return Vago;
+    }
+
+    public void setVago(String vago) {
+        Vago = vago;
+    }
+
+    public String getPanvas() {
+        return Panvas;
+    }
+
+    public void setPanvas(String panvas) {
+        Panvas = panvas;
+    }
+
+    public String getNumeroEndereco() {
+        return NumeroEndereco;
+    }
+
+    public void setNumeroEndereco(String numeroEndereco) {
+        NumeroEndereco = numeroEndereco;
+    }
+
+    public String getComplementoEndereco() {
+        return ComplementoEndereco;
+    }
+
+    public void setComplementoEndereco(String complementoEndereco) {
+        ComplementoEndereco = complementoEndereco;
+    }
+}
