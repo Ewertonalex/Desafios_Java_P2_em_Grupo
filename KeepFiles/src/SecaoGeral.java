759939aee636d6e51fd4d478ebd1623cbd4a4c10
Luan Antony <luanantony29@gmail.com>
2022-05-21 16:53:09 -0300

    Ajustando validações SeçãoGeral e SeçãoLote

--- KeepFiles/src/SecaoGeral.java
+++ KeepFiles/src/SecaoGeral.java
@@ -10,7 +10,7 @@ public class SecaoGeral {
     public SecaoGeral(String linhaArquivo) {
         this.TipRegHg = linhaArquivo.substring(0,1);
         this.Banco = linhaArquivo.substring(1,5);
-        this.Nsp = linhaArquivo.substring(5,12);
+        this.Nsp = linhaArquivo.substring(5,13);
         this.SistOrig = linhaArquivo.substring(13,32);
         this. DtMovto = linhaArquivo.substring(32,40);
     }
